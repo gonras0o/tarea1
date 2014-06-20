0ecb71a9210dc0be0ce474cfa62abcced1193a43
jtomas23 <Tomas@MacBook-Pro-de-Jose-4.local>
2014-06-20 00:08:44 -0400

    GUI
    ermmm no es lo mas bonito, pero es mejor que numeritos en la consola y
    es algo mas entendible… no tiene mucho sentido imprimir tooooodas las
    tablas, pero por si acaso lo hago igual, para que se vea que son
    correctas

--- Tarea 3/Codigo Python Script 2 en 1/TablaCostos2en1.py
+++ Tarea 3/Codigo Python Script 2 en 1/TablaCostos2en1.py
@@ -1,3 +1,4 @@
+from Tkinter import *
 
 class PC:
 	def __init__(self):
@@ -10,25 +11,32 @@ class PC:
 		Router("G",VectoresDistancia=[4,-1,-1,-1,-1,-1,-1,7,-1],Vecinos = [0,7],digitoPos=6),
 		Router("H",VectoresDistancia=[-1,-1,-1,-1,-1,6,7,-1,3],Vecinos = [5,6,8],digitoPos=7),
 		Router("I",VectoresDistancia=[10,-1,-1,2,1,-1,-1,3,-1],Vecinos = [0,3,4,7],digitoPos=8)]
-
 		NodosActualizados=[7,8]
 		ValoresNuevo=0
 		ValorAnterior=3
+
 		for j in range(9):
 			self.CalculoTablaCostosInicial()
 			self.CalculoTablaCostosFinal()
-		self.printTablaFinal()
+
+		self.Aviso1()
+		self.printTablaPrincipal()
+
 		self.Routers[7].Vecinos.pop()
 		self.Routers[8].Vecinos.pop()
 		self.Routers[7].VectoresDistancia[8]=0
 		self.Routers[8].VectoresDistancia[7]=0
+
 		for z in range(9):
 			self.Routers[z].RellenaTablaInicial()
 		for i in range(9):
 			self.CalculoTablaCostosInicial()
 			self.CalculoTablaCostosFinal()
+
 		self.AjusteMatrizDiagonal()
-		self.printTablaFinal()
+		self.Aviso2()
+		self.printTablaNodoCortado()
+		
 
 	def PrintTablas(self):
 		for n in self.Routers:
@@ -67,21 +75,100 @@ class PC:
 					elif(n==NodosActualizados[1]):
 						r.TablaCostos[r.digitoPos][7]=0
 						r.TablaCostos[7][r.digitoPos]=0
-				"""
-				j=1
-				for i in NodosActualizados:
-					for n in self.Routers[i].Vecinos:
-						if(self.Routers[n].TablaCostos[n][NodosActualizados[j]]==self.Routers[n].TablaCostos[n][i]+ValorAnterior):
-							self.Routers[n].TablaCostos[n][j]=0
-					j=0
-					"""
 
-	def printTablaFinal(self):
+	def printTablaPrincipal(self):
+		labels = [None] * 9
+		ventana = [None] * 9
+		labelFrom = [None] * 9
+		labelTo = [None] * 9
+		labelFromLt = [None] * 9
+		labelToLt = [None] * 9
+
+		for n in range(9):
+			labels[n] = [None] * 9
+
 		for r in self.Routers:
-			print "tabla "+r.Id,r.digitoPos
+			ventana[r.digitoPos] = Tk()
+			ventana[r.digitoPos].title("Tabla de Costos router: "+r.Id)
+			
 			for i in range(9):
-				print r.TablaCostos[i]
-			print "\n"
+				labelFrom[i]=Label(ventana[r.digitoPos],text="From")
+				labelFrom[i].grid(row=2,column=1)
+				for z in range(9):
+					labelFromLt[z]=Label(ventana[r.digitoPos],text=self.Routers[z].Id)
+					labelFromLt[z].grid(row=z+3,column=1)
+					labelToLt[z]=Label(ventana[r.digitoPos],text=self.Routers[z].Id)
+					labelToLt[z].grid(row=1,column=z+3)
+
+				labelTo[i]=Label(ventana[r.digitoPos],text="Cost To")
+				labelTo[i].grid(row=1,column=2)
+				for j in range(9):
+					a="     "
+					labels[i][j]=Label(ventana[r.digitoPos],text=str(r.TablaCostos[i][j])+a)
+					labels[i][j].grid(row=i+3,column=j+3)
+
+			finish = Button(ventana[r.digitoPos],text="Cerrar",relief=FLAT, command=lambda:self.destruir(ventana[r.digitoPos]))
+			finish.grid(row=i+6,column=j/2+2)
+			ventana[r.digitoPos].mainloop()
+
+	def printTablaNodoCortado(self):
+		labels = [None] * 9
+		ventana = [None] * 9
+		labelFrom = [None] * 9
+		labelTo = [None] * 9
+		labelFromLt = [None] * 9
+		labelToLt = [None] * 9
+
+		for n in range(9):
+			labels[n] = [None] * 9
+
+		for r in self.Routers:
+			ventana[r.digitoPos] = Tk()
+			ventana[r.digitoPos].title("Tabla de Costos router Luego del Corte H <-> I: "+r.Id)
+			
+			for i in range(9):
+				labelFrom[i]=Label(ventana[r.digitoPos],text="From")
+				labelFrom[i].grid(row=2,column=1)
+				for z in range(9):
+					labelFromLt[z]=Label(ventana[r.digitoPos],text=self.Routers[z].Id)
+					labelFromLt[z].grid(row=z+3,column=1)
+					labelToLt[z]=Label(ventana[r.digitoPos],text=self.Routers[z].Id)
+					labelToLt[z].grid(row=1,column=z+3)
+
+				labelTo[i]=Label(ventana[r.digitoPos],text="To")
+				labelTo[i].grid(row=1,column=2)
+				for j in range(9):
+					a="     "
+					labels[i][j]=Label(ventana[r.digitoPos],text=str(r.TablaCostos[i][j])+a)
+					labels[i][j].grid(row=i+3,column=j+3)
+
+			finish = Button(ventana[r.digitoPos],text="Cerrar",relief=FLAT, command=lambda:self.destruir(ventana[r.digitoPos]))
+			finish.grid(row=i+6,column=j/2+2)
+			ventana[r.digitoPos].mainloop()
+
+	def Aviso1(self):
+		ventana = Tk()
+		ventana.title("AVISOOOOOOO 1")
+		label=Label(ventana,text="Aviso: las ventanas a continuacion contienen las tablas de costo de todos los Routers")
+		label.grid(row=1,column=1)
+		finish = Button(ventana,text="Cerrar",relief=FLAT, command=lambda:self.destruir(ventana))
+		finish.grid(row=2,column=5)
+		ventana.mainloop()
+
+	def Aviso2(self):
+		ventana = Tk()
+		ventana.title("AVISOOOOOOO 2")
+		label1=Label(ventana,text="Aviso 2: Las siguientes ventanas contienen las tablas de costo")
+		label1.grid(row=1,column=1)
+		label2=Label(ventana,text="         despues de que un trasatlantico cortara la conexion entre el nodo H <-> I")
+		label2.grid(row=2,column=1)
+		finish = Button(ventana,text="Cerrar",relief=FLAT, command=lambda:self.destruir(ventana))
+		finish.grid(row=3,column=5)
+		ventana.mainloop()
+
+	def destruir(self,ventana):
+		ventana.destroy()
+
 	def AjusteMatrizDiagonal(self):
 		for r in self.Routers:
 			for i in range(9):
